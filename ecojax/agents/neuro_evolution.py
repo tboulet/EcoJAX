b7dc4a4e1adde8ac4f90bdcd609f565b101fb198
tboulet <timothe.boulet0@gmail.com>
2024-06-28 15:38:09 +0200

    add ne to framrwork

--- ecojax/agents/neuro_evolution.py
+++ ecojax/agents/neuro_evolution.py
@@ -13,72 +13,94 @@ import flax.linen as nn
 from ecojax.agents.base_agent_species import AgentSpecies
 from ecojax.core.eco_info import EcoInformation
 from ecojax.models.base_model import BaseModel
-from ecojax.evolution.mutator import mutation_gaussian_noise
+from ecojax.evolution.mutator import mutate_scalar, mutation_gaussian_noise
 from ecojax.types import ActionAgent, ObservationAgent
 import ecojax.spaces as spaces
 from ecojax.utils import jprint
 
 
 @struct.dataclass
-class StateAgentEvolutionary:
+class HyperParametersNE:
+    # The mutation strength of the agent
+    strength_mutation: float
+
+
+@struct.dataclass
+class AgentNE:
     # The age of the agent, in number of timesteps
     age: int
 
     # The parameters of the neural network corresponding to the agent
     params: Dict[str, jnp.ndarray]
 
+    # Hyperparameters of the agent
+    hp: HyperParametersNE
+
+
+@struct.dataclass
+class StateSpeciesNE:
+    # The agents of the species
+    agents: AgentNE
+
 
 class NeuroEvolutionAgentSpecies(AgentSpecies):
     """A species of agents that evolve their neural network weights."""
 
-    def reset(self, key_random: jnp.ndarray) -> None:
-
+    def reset(self, key_random: jnp.ndarray) -> StateSpeciesNE:
         # Initialize the state
-        def init_single_agent(
-            model: BaseModel,
-            key_random: jnp.ndarray,
-        ) -> jnp.ndarray:
-            variables = model.get_initialized_variables(key_random)
-            params = variables.get("params", {})
-            return StateAgentEvolutionary(age=0, params=params)
-
         key_random, subkey = random.split(key_random)
         batch_keys = jax.random.split(subkey, self.n_agents_max)
-        init_many_agents = jax.vmap(init_single_agent, in_axes=(None, 0))
-        self.batch_state_agents = init_many_agents(
-            self.model,
-            batch_keys,
+        batch_agents = jax.vmap(self.init_agent)(batch_keys)
+        return StateSpeciesNE(
+            agents=batch_agents,
         )
 
+    def init_hp(self) -> HyperParametersNE:
+        """Get the initial hyperparameters of the agent from the config"""
+        return HyperParametersNE(**self.config["hp_initial"])
+
+    def init_agent(
+        self,
+        key_random: jnp.ndarray,
+        params: Dict[str, jnp.ndarray] = None,
+        hp: HyperParametersNE = None,
+    ) -> AgentNE:
+        """Create a new agent.
+
+        Args:
+            key_random (jnp.ndarray): the random key
+            params (Dict[str, jnp.ndarray], optional): the NN parameters. Defaults to None (will be initialized randomly)
+            hp (HyperParametersRL, optional): the hyperparameters of the agent. Defaults to None (will be initialized from the config).
+
+        Returns:
+            AgentRL: the new agent
+        """
+        if params is None:
+            variables = self.model.get_initialized_variables(key_random)
+            params = variables.get("params", {})
+        if hp is None:
+            hp = self.init_hp()
+        return AgentNE(
+            age=0,
+            params=params,
+            hp=hp,
+        )
+        
     def react(
         self,
-        key_random: jnp.ndarray,
+        state: StateSpeciesNE,
         batch_observations: ObservationAgent,  # Batched
         eco_information: EcoInformation,
-    ) -> ActionAgent:
-
-        self.batch_state_agents, batch_actions = self.react_jitted(
-            key_random=key_random,
-            batch_state_agents=self.batch_state_agents,
-            batch_observations=batch_observations,
-            eco_information=eco_information,
-        )
-
-        return batch_actions
-
-    @partial(jax.jit, static_argnums=(0,))
-    def react_jitted(
-        self,
         key_random: jnp.ndarray,
-        batch_state_agents: StateAgentEvolutionary,  # Batched
-        batch_observations: ObservationAgent,  # Batched
-        eco_information: EcoInformation,
-    ) -> jnp.ndarray:
+    ) -> Tuple[
+        StateSpeciesNE,
+        ActionAgent,  # Batched
+    ]:
 
         # Apply the mutation
         batch_keys = random.split(key_random, self.n_agents_max)
-        batch_state_agents_mutated = jax.vmap(self.mutate_state_agent)(
-            batch_state_agents, key_random=batch_keys
+        batch_agents_mutated = jax.vmap(self.mutate_state_agent)(
+            agent=state.agents, key_random=batch_keys
         )
 
         # Transfer the genes from the parents to the childs component by component using jax.tree_map
@@ -116,34 +138,43 @@ class NeuroEvolutionAgentSpecies(AgentSpecies):
             )
             return genes_inherited
 
-        new_batch_state_agents = tree_map(
+        new_batch_agents = tree_map(
             manage_genetic_component_inheritance,
-            batch_state_agents,
-            batch_state_agents_mutated,
+            state.agents,
+            batch_agents_mutated,
+        )
+
+        new_state_species = StateSpeciesNE(
+            agents=new_batch_agents,
         )
 
         # Agent-wise reaction
         key_random, subkey = random.split(key_random)
-        new_batch_state_agents, batch_actions = self.act_agents(
+        new_state_species, batch_actions = self.act_agents(
             key_random=subkey,
+            state_species=new_state_species,
             batch_observations=batch_observations,
-            batch_state_agents=new_batch_state_agents,
         )
 
-        return new_batch_state_agents, batch_actions
+        return new_state_species, batch_actions
+
 
     # =============== Mutation methods =================
 
     def mutate_state_agent(
-        self, state_agent: StateAgentEvolutionary, key_random: jnp.ndarray
-    ) -> StateAgentEvolutionary:
-        return state_agent.replace(
+        self, agent: AgentNE, key_random: jnp.ndarray
+    ) -> AgentNE:
+        key_random, *subkeys = random.split(key_random, 5)
+        return agent.replace(
             age=0,
             params=mutation_gaussian_noise(
-                arr=state_agent.params,
-                strength_mutation=0.001,
+                arr=agent.params,
+                strength_mutation=agent.hp.strength_mutation,
                 key_random=key_random,
             ),
+            hp=HyperParametersNE(strength_mutation=mutate_scalar(
+                value=agent.hp.strength_mutation, range=(0, None), key_random=subkeys[3]
+            ))
         )
 
     # =============== Agent creation methods =================
@@ -151,14 +182,14 @@ class NeuroEvolutionAgentSpecies(AgentSpecies):
     def act_agents(
         self,
         key_random: jnp.ndarray,
+        state_species: StateSpeciesNE,
         batch_observations: ObservationAgent,  # Batched
-        batch_state_agents: StateAgentEvolutionary,  # Batched
     ) -> jnp.ndarray:
 
         def act_single_agent(
             key_random: jnp.ndarray,
+            state_agent: AgentNE,
             obs: jnp.ndarray,
-            state_agent: StateAgentEvolutionary,
         ) -> jnp.ndarray:
             # Inference part
             action, prob_action = self.model.apply(
@@ -168,15 +199,18 @@ class NeuroEvolutionAgentSpecies(AgentSpecies):
             )
             # Learning part
             state_agent.replace(age=state_agent.age + 1)
+            pass  # TODO: Implement the learning part
             # Update the agent's state and act
             return state_agent, action
 
-        act_many_agents = jax.vmap(act_single_agent, in_axes=(0, 0, 0))
         batch_keys = random.split(key_random, self.n_agents_max)
-        batch_state_agents, batch_actions = act_many_agents(
-            batch_keys,
-            batch_observations,
-            batch_state_agents,
+        batch_agents, batch_actions = jax.vmap(act_single_agent)(
+            key_random=batch_keys,
+            state_agent=state_species.agents,
+            obs=batch_observations,
         )
 
-        return batch_state_agents, batch_actions
+        new_state_species = StateSpeciesNE(
+            agents=batch_agents,
+        )
+        return new_state_species, batch_actions

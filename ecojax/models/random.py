bc583b0b70a86434bb3de0907984337bf71550b6
Timothe Boulet <timothe.boulet0@gmail.com>
2024-08-29 12:10:10 +0200

    add random

--- ecojax/models/random.py
+++ ecojax/models/random.py
@@ -14,35 +14,8 @@ from ecojax.spaces import ContinuousSpace, DiscreteSpace
 
 class RandomModel(BaseModel):
 
-    def __call__(self, obs: ObservationAgent, key_random: jnp.ndarray) -> ActionAgent:
-        # Generate the outputs for each action space
-        kwargs_action = {}
-        prob_action_sampled = 1.0
-
-        for name_action_component, space in self.action_space_dict.items():
-            key_random, subkey = random.split(key_random)
-
-            if isinstance(space, DiscreteSpace):
-                action_component_sampled = random.randint(
-                    subkey, shape=(), minval=0, maxval=space.n
-                )
-                action_component_prob = 1.0 / space.n
-
-            elif isinstance(space, ContinuousSpace):
-                action_component_sampled = random.uniform(
-                    subkey,
-                    shape=space.shape,
-                    minval=space.low,
-                    maxval=space.high,
-                )
-                action_component_prob = 1 / (
-                    np.prod(space.shape) * (space.high - space.low)
-                )
-            else:
-                raise ValueError(f"Unknown space type for action: {type(space)}")
-
-            kwargs_action[name_action_component] = action_component_sampled
-            prob_action_sampled *= action_component_prob
-
-        # Return the action
-        return self.action_class(**kwargs_action), prob_action_sampled
+    def obs_to_encoding(
+        self, obs: ObservationAgent, key_random: jnp.ndarray
+    ) -> jnp.ndarray:
+        """Converts the observation to a vector encoding that can be processed by the MLP."""
+        return self.space_output.sample(key_random)
